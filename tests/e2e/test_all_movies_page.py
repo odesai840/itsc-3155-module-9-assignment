6daa2c63da5c21c095e3ffda8340e3bf7a78acd5
odesai840 <odesai840@gmail.com>
2023-10-22 01:55:07 -0400

    table edited to work with a dict instead of a list (my bad) and tests completed

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,15 @@
 # TODO: Feature 1
+from flask.testing import FlaskClient
+
+def test_create_movie(test_app: FlaskClient):
+    # Send a GET request to the '/movies' route
+    response = test_app.get('/movies')
+    assert response.status_code == 200
+
+    # Send a POST request with test data
+    response = test_app.post('/movies', data={
+        'title': 'SomeTitle1',
+        'director': 'SomeDirector1',
+        'rating': 5
+    })
+    assert response.status_code == 302
