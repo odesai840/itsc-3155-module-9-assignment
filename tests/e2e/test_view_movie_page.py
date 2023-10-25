04a3f397dd8959ce7fcdba41e22eb8b882b1f027
java-Bean <110148625+java-Bean@users.noreply.github.com>
2023-10-25 19:32:54 -0400

    Added code to test_view_movie_page.py, test_get_movie_by_id.py, and app.py for exercise 4.

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,1 +1,27 @@
-# TODO: Feature 4
+
+import pytest
+from flask import url_for
+from src.repositories.movie_repository import get_movie_repository
+
+
+
+
+# Test viewing a single movie
+@pytest.mark.parametrize('movie_id, expected_status_code', [
+    (1, 200),  # Existing movie
+    (9999, 404)  # Non-existing movie
+])
+
+def test_view_single_movie(client, movie_id, expected_status_code):
+    response = client.get(url_for('view_single_movie', movie_id=movie_id))
+    assert response.status_code == expected_status_code
+
+# Test deleting a movie
+@pytest.mark.parametrize('movie_id, expected_status_code', [
+    (1, 204),  # Existing movie
+    (9999, 404)  # Non-existing movie
+])
+
+def test_delete_movie(client, movie_id, expected_status_code):
+    response = client.delete(url_for('delete_movie', movie_id=movie_id))
+    assert response.status_code == expected_status_code
--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,38 @@
+
+import pytest
+from src.repositories.movie_repository import get_movie_repository
+
 # TODO: Feature 4
+
+# Initialize mock data
+mock_movie_data = {
+    'id': 1,
+    'title': 'Inception',
+    'director': 'Christopher Nolan',
+    'genre': 'Sci-Fi',
+    'release_year': 2010
+}
+
+def test_get_movie_by_id():
+    # Add mock data to repository
+    movie_repository.add_movie(mock_movie_data)
+    
+    # Test get_movie_by_id method
+    movie = movie_repository.get_movie_by_id(1)
+    assert movie['id'] == 1
+    assert movie['title'] == 'Inception'
+    assert movie['director'] == 'Christopher Nolan'
+    assert movie['genre'] == 'Sci-Fi'
+    assert movie['release_year'] == 2010
+
+def test_delete_movie_by_id():
+    # Add mock data to repository
+    movie_repository.add_movie(mock_movie_data)
+    
+    # Test delete_movie_by_id method
+    success = movie_repository.delete_movie_by_id(1)
+    assert success == True
+    
+    # Verify movie is deleted
+    movie = movie_repository.get_movie_by_id(1)
+    assert movie is None
