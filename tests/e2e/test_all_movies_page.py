52dd96656352426b79e3070aa67307163ccc46ff
odesai840 <odesai840@gmail.com>
2023-10-25 19:51:45 -0400

    added bad route tests and some new comments

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,11 +1,13 @@
 # TODO: Feature 1
 from flask.testing import FlaskClient
+from src.repositories import movie_repository
 
 def test_create_movie(test_app: FlaskClient):
-    # Send a GET request to the '/movies' route
+    # send a GET request to the '/movies' route
     response = test_app.get('/movies')
     assert response.status_code == 200
 
+    # send a POST request to create a new movie
     response = test_app.post('/movies', data = {
         'movieName': 'TestTitle',
         'director': 'TestDirector',
@@ -13,6 +15,11 @@ def test_create_movie(test_app: FlaskClient):
     }, follow_redirects=True)
     assert response.status_code == 200
 
+    # check if the movie got added to the table
     assert b'TestTitle' in response.data
     assert b'TestDirector' in response.data
     assert b'5' in response.data
+
+    # bad tests
+    assert b'BadTitle' not in response.data
+    assert b'BadDirector' not in response.data
